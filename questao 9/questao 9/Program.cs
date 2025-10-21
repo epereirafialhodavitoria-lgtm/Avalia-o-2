955d5665781f59ba8b1a392136a2726c437bd653
epereirafialhodavitoria-lgtm <epereirafialhodavitoria@gmail.com>
2025-10-21 09:23:01 -0300

    Add files via upload

---
+++ questao 9/questao 9/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] nomes = { "Ana", "Eduarda", "Alice", "Julia", "Bruno" };
+
+Console.WriteLine("Digite um nome para pesquisa");
+string nomeBusca = Console.ReadLine();
+bool encontrado  = false;
+
+foreach (string nome in nomes)
+{
+    if (nome.Equals(nomeBusca, StringComparison.OrdinalIgnoreCase))
+    {
+        encontrado = true;
+        break;
+    }
+       
+}
+if (encontrado)
+{
+    Console.WriteLine($"Nome'{nomeBusca}' foi encontrado no vetor");
+}
+else
+{
+    Console.WriteLine($"Nome '{nomeBusca}' não foi encontrado no vetor" );
+}
